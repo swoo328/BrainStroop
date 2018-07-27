ea6fc3856d37440da3f048cd9f84ae81c066c61f
Steven Woo <swoo328@yahoo.com>
2018-07-26 22:36:43 -0400

    Initial commit

---
+++ app/src/main/java/com/example/swoo3/brainstroop/BrainStroop.java
@@ -0,0 +1,13 @@
+package com.example.swoo3.brainstroop;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class BrainStroop extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_brain_stroop);
+    }
+}
